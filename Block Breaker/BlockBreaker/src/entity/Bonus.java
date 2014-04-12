e3fd262406bbda15c6d0e36bc2b531371e338ee6
Alp Kaan Erer <alp.erer@ozu.edu.tr>
2014-04-12 20:15:25 +0300

    Bonus Class Implementation Started

--- Block Breaker/BlockBreaker/src/entity/Bonus.java
+++ Block Breaker/BlockBreaker/src/entity/Bonus.java
@@ -1,10 +1,33 @@
 package entity;
 
- /*
- *Author: Can Sahin
- */
+import java.awt.Color;
+import java.awt.Image;
 
 public class Bonus {
-
+	
+	private int bonusType;
+	private int xLoc;
+	private int yLoc;
+	private int width = 10;
+	private int height = 10;
+	private int speed = -2;
+	private Color bonusColor;
+	
+	public Bonus(int xLoc, int yLoc, int bonusType){
+		this.xLoc = xLoc;
+		this.yLoc = yLoc;
+		this.bonusType = bonusType;
+	}
+	
+	private void move(){
+		this.yLoc += speed;
+	}
+	
+	private void drawBonus(){
+		if(bonusType == 1) {
+			bonusColor = Color.red;
+		}
+	}
+	
 }
 
