38256091cb28d55c1b6a9623be0e7e1bef1a46a9
Alp Kaan Erer <alp.erer@ozu.edu.tr>
2014-04-12 21:04:18 +0300

    Ball and Bonus classes are implemented

--- Block Breaker/BlockBreaker/src/entity/Ball.java
+++ Block Breaker/BlockBreaker/src/entity/Ball.java
@@ -1,10 +1,31 @@
 package entity;
 
- /*
- *Author: Can Sahin
- */
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 
-public class Ball {
+public class Ball extends Rectangle {
 
+	private Point position;
+	private BufferedImage image;
+	private int ballRadius = 20;
+
+	private Ball createBall(Point position){
+		Ball ball = new Ball();
+		ball.setBounds(position.x, position.y, ballRadius, ballRadius);
+		ball.setImage(image);
+		return ball;
+	}
+	
+	private void setImage(BufferedImage image) {
+		this.image = image;
+	}
+	
+	private void moveBall(int xSpeed, int ySpeed){
+		this.position.x += xSpeed;
+		this.position.y += ySpeed;		
+	}
+	
 }
 
--- Block Breaker/BlockBreaker/src/entity/Bonus.java
+++ Block Breaker/BlockBreaker/src/entity/Bonus.java
@@ -1,10 +1,39 @@
 package entity;
 
- /*
- *Author: Can Sahin
- */
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 
-public class Bonus {
 
+public class Bonus extends Rectangle {
+
+	private int bonusType;
+	private Point position;
+	private BufferedImage image;
+	private int width = 100;
+	private int height = 10;
+	private int speed = -2;
+	private Color bonusColor;
+
+	
+	private Bonus createBonus(Point position, int bonusType){
+		Bonus bonus = new Bonus();
+		bonus.setLocation(position);
+		bonus.setSize(width, height);
+		setColor(bonus, bonusType);
+		return bonus;
+	}
+
+	private void setColor(Bonus bonus, int bonusType2) {
+		if(bonusType == 1){
+			
+		}
+	}
+
+	private void moveBonus(){
+		this.position.y += speed;
+	}
+	
 }
 
