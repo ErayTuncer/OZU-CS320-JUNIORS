10c5ad9414699a2703177987f4774bc94268899d
ErayT <eray.tuncer@ozu.edu.tr>
2014-04-13 18:36:42 +0300

    GameView draw methods added

--- Block Breaker/BlockBreaker/src/graphic/view/GameView.java
+++ Block Breaker/BlockBreaker/src/graphic/view/GameView.java
@@ -1,10 +1,13 @@
 package graphic.view;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Point;
 import java.util.ArrayList;
 
 import controller.GameController;
 import entity.Brick;
+import entity.Paddle;
 
 
 public class GameView extends View {
@@ -17,17 +20,37 @@ public class GameView extends View {
 	
 	@Override
 	protected void paintComponent(Graphics graphics) {
-		// TODO implement
+		// TODO add bonus 
 		super.paintComponent(graphics);
-		drawBricks(graphics);		
+		drawBricks(graphics);
+		drawPaddle(graphics);
+		drawBall(graphics);
 	}
 
 	private void drawBricks(Graphics graphics) {
 		ArrayList<Brick> bricks = controller.getLevel().getBricks();
 		
+		for (Brick brick : bricks) {
+			Point position = brick.getPosition();
+			Dimension size = brick.getSize(); 
+			
+			graphics.drawImage(brick.getImage(), position.x, position.y, size.width, size.height, null);		
+		}
 	}
-
 	
+	private void drawPaddle(Graphics graphics) {
+		Paddle paddle = controller.getLevel().getPaddle();
+		Point position = paddle.getPosition();
+		Dimension size = paddle.getSize();
+		
+		graphics.setColor(paddle.getColor());
+		graphics.drawRect(position.x, position.y, size.width, size.height);
+	}
+	
+	private void drawBall(Graphics graphics) {
+		//TODO : finish
+		
+	}
 	
 }
 
